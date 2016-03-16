3478b3a70497edaa4f5eb2d9e4f2551859691c69
Ben Wiley <therealbenwiley@gmail.com>
2016-03-15 22:32:18 -0400

    Full jsx/sass for player included (without interactivity).

--- src/index.js
+++ src/index.js
@@ -12,7 +12,36 @@ class AudioPlayer extends React.Component {
 
   render () {
     return (
-      <div className="audio_player">Audio Player</div>
+      <div id="audio_player" className="audio_player">
+
+        <div className="audio_controls">
+          <div id="play_pause_button" className="play_pause_button audio_button paused">
+            <div className="play_pause_inner">
+              <div className="left"></div>
+              <div className="right"></div>
+              <div className="triangle_1"></div>
+              <div className="triangle_2"></div>
+            </div>
+          </div>
+          <div id="skip_button" className="skip_button audio_button">
+            <div className="skip_button_inner">
+              <div className="right_facing_triangle"></div>
+              <div className="right_facing_triangle"></div>
+            </div>
+          </div>
+        </div>
+
+        <div id="audio_progress_container" className="audio_progress_container">
+          <div id="audio_progress" className="audio_progress"></div>
+          <div id="audio_progress_overlay" className="audio_progress_overlay">
+            <div className="audio_info_marquee">
+              <div id="audio_info" className="audio_info noselect" draggable="false"></div>
+            </div>
+            <div id="audio_time_progress" className="audio_time_progress noselect" draggable="false"></div>
+          </div>
+        </div>
+
+      </div>
     );
   }
 
