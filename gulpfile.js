9c8a99a4e7e57b80e8ecfaf6fc30723b694145a8
Ben Wiley <therealbenwiley@gmail.com>
2016-03-15 21:55:06 -0400

    gh-pages site working with loaded React module.

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var plumber = require('gulp-plumber');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var minifyCSS = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('copy', [
+  'copy:html'
+]);
+
+gulp.task('copy:html', function () {
+  return gulp.src('./src/**/*.html')
+    .pipe(gulp.dest('./dist/'));
+});
+ 
+gulp.task('browserify', function() {
+  return browserify('./src/js/index.js')
+    .transform('babelify', {presets: ['es2015', 'react']})
+    .bundle()
+    .on('error', function (err) {
+      console.error(err);
+      this.emit('end');
+    })
+    .pipe(source('main.js'))
+    .pipe(gulp.dest('./dist/js/'));
+});
+
+gulp.task('styles', [
+  'styles:main',
+  'styles:audioplayer'
+]);
+
+gulp.task('styles:audioplayer', function () {
+  return gulp.src('./node_modules/react-responsive-audio-player/dist/bundle.css')
+    .pipe(plumber())
+    .pipe(minifyCSS())
+    .pipe(rename('audioplayer.css'))
+    .pipe(gulp.dest('./dist/css/'))
+});
+
+gulp.task('styles:main', function () {
+  return gulp.src('*.css', { cwd: 'src/css/' })
+    .pipe(plumber())
+    .pipe(concat('all.css'))
+    .pipe(autoprefixer({ browsers: ['> 2%'] }))
+    .pipe(minifyCSS())
+    .pipe(rename('main.css'))
+    .pipe(gulp.dest('dist/css/'), { cwd: '../../' });
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['src/**/*.html'], ['copy:html']);
+  gulp.watch('src/css/*.css', ['styles:main']);
+  gulp.watch(['src/js/**/*.js'], ['browserify']);
+});
+
+gulp.task('default', ['copy', 'browserify', 'styles', 'watch']);
---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const AudioPlayer = require('react-responsive-audio-player');
+
+ReactDOM.render(
+  <AudioPlayer/>,
+  document.getElementById('audio_player_container')
+);
