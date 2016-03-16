b11f4068991d7679257909df035a300525355d5c
Ben Wiley <therealbenwiley@gmail.com>
2016-03-16 02:25:54 -0400

    playlist prop added to audioplayer, and unnecessary gulp packages removed.

--- dist/main.js
+++ dist/main.js
@@ -19037,7 +19037,19 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var AudioPlayer = require('react-responsive-audio-player');
 
-ReactDOM.render(React.createElement(AudioPlayer, null), document.getElementById('audio_player_container'));
+var dir = 'audio/';
+var playlist = [{
+  url: dir + 'marty_mcpaper_theme.mp3',
+  displayText: 'Marty McPaper: Epic Delivery Service'
+}, {
+  url: dir + 'secret_of_trash_island.mp3',
+  displayText: 'The Secret of Trash Island'
+}, {
+  url: dir + 'in_the_hall_of_the_mountain_king.mp3',
+  displayText: 'In the Hall of the Mountain King'
+}];
+
+ReactDOM.render(React.createElement(AudioPlayer, { playlist: playlist }), document.getElementById('audio_player_container'));
 
 },{"react":158,"react-dom":2,"react-responsive-audio-player":160}],160:[function(require,module,exports){
 (function webpackUniversalModuleDefinition(root, factory) {
@@ -19107,6 +19119,32 @@ return /******/ (function(modules) { // webpackBootstrap
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 	var React = __webpack_require__(1);
+	var classNames = __webpack_require__(2);
+
+	var log = console.log.bind(console);
+	var logError = console.error ? console.error.bind(console) : log;
+	var logWarning = console.warn ? console.warn.bind(console) : log;
+
+	/* converts given number of seconds to standard time display format
+	 * http://goo.gl/kEvnKn
+	 */
+	function convertToTime(number) {
+	  var mins = Math.floor(number / 60);
+	  var secs = (number % 60).toFixed();
+	  return '' + (mins < 10 ? '0' : '') + mins + ':' + (secs < 10 ? '0' : '') + secs;
+	}
+
+	/*
+	 * AudioPlayer
+	 *
+	 * Accepts 'playlist' prop of the form:
+	 *
+	 * [{ "url": "./path/to/file.mp3",
+	 *    "displayText": "ArtistA - Track 1" },
+	 *  { "url": "https://domain.com/track2.ogg",
+	 *    "displayText": "ArtistB - Track 2" }]
+	 *
+	 */
 
 	var AudioPlayer = function (_React$Component) {
 	  _inherits(AudioPlayer, _React$Component);
@@ -19114,21 +19152,271 @@ return /******/ (function(modules) { // webpackBootstrap
 	  function AudioPlayer(props) {
 	    _classCallCheck(this, AudioPlayer);
 
-	    return _possibleConstructorReturn(this, Object.getPrototypeOf(AudioPlayer).call(this, props));
+	    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(AudioPlayer).call(this, props));
+
+	    _this.playlist = props.playlist;
+
+	    /* true if the user is currently dragging the mouse
+	     * to seek a new track position
+	     */
+	    _this.seekInProgress = false;
+	    // index matching requested track (whether track has loaded or not)
+	    _this.currentTrackIndex = 0;
+
+	    _this.state = {
+	      /* activeTrackIndex will change to match
+	       * this.currentTrackIndex once metadata has loaded
+	       */
+	      activeTrackIndex: -1,
+	      // indicates whether audio player should be paused
+	      paused: true,
+	      // elapsed time for current track, in seconds
+	      displayedTime: 0
+	    };
+
+	    // html audio element used for playback
+	    _this.audio = null;
+	    _this.audioProgressContainer = null;
+	    /* bounding rectangle used for calculating seek
+	     * position from mouse/touch coordinates
+	     */
+	    _this.audioProgressBoundingRect = null;
+	    return _this;
 	  }
 
 	  _createClass(AudioPlayer, [{
 	    key: 'componentDidMount',
 	    value: function componentDidMount() {
-	      __webpack_require__(2);
+	      var _this2 = this;
+
+	      __webpack_require__(3);
+
+	      // These listeners are outside the scope of our render method
+	      window.addEventListener('mouseup', this.seek.bind(this));
+	      document.addEventListener('touchend', this.seek.bind(this));
+	      window.addEventListener('resize', function () {
+	        _this2.audioProgressBoundingRect = _this2.audioProgressContainer.getBoundingClientRect();
+	      });
+
+	      /* We'll need to use some tools outside of the React
+	       * paradigm in order to hook up audio things correctly.
+	       */
+	      var audio = this.audio = document.createElement('audio');
+	      audio.preload = 'metadata';
+	      audio.addEventListener('ended', this.skipToNextTrack.bind(this));
+	      audio.addEventListener('timeupdate', this.handleTimeUpdate.bind(this));
+	      audio.addEventListener('loadedmetadata', function () {
+	        _this2.setState({
+	          activeTrackIndex: _this2.currentTrackIndex
+	        });
+	      });
+	      audio.addEventListener('play', function () {
+	        _this2.setState({
+	          paused: false
+	        });
+	      });
+	      audio.addEventListener('stalled', this.togglePause.bind(this, true));
+	      if (this.playlist) {
+	        this.updateSource();
+	      }
+	    }
+	  }, {
+	    key: 'componentWillReceiveProps',
+	    value: function componentWillReceiveProps(nextProps) {
+	      if (!nextProps.playlist) {
+	        return;
+	      }
+	      this.playlist = nextProps.playlist;
+	      this.currentTrackIndex = -1;
+	      if (this.audio) {
+	        this.skipToNextTrack(false);
+	      }
+	    }
+	  }, {
+	    key: 'togglePause',
+	    value: function togglePause(value) {
+	      var pause = typeof value === 'boolean' ? value : !this.state.paused;
+	      if (pause) {
+	        this.audio.pause();
+	        return this.setState({
+	          paused: true
+	        });
+	      }
+	      try {
+	        this.audio.play();
+	      } catch (error) {
+	        logError(error);
+	        var warningMessage = 'Audio playback failed at ' + new Date().toLocaleTimeString() + '! (Perhaps autoplay is disabled in this browser.)';
+	        logWarning(warningMessage);
+	      }
+	    }
+	  }, {
+	    key: 'skipToNextTrack',
+	    value: function skipToNextTrack(shouldPlay) {
+	      var _this3 = this;
+
+	      this.audio.pause();
+	      var i = this.currentTrackIndex + 1;
+	      if (i >= this.playlist.length) {
+	        i = 0;
+	      }
+	      this.currentTrackIndex = i;
+	      this.setState({
+	        activeTrackIndex: -1,
+	        displayedTime: 0
+	      }, function () {
+	        _this3.updateSource();
+	        var shouldPause = typeof shouldPlay === 'boolean' ? !shouldPlay : false;
+	        _this3.togglePause(shouldPause);
+	      });
+	    }
+	  }, {
+	    key: 'updateSource',
+	    value: function updateSource() {
+	      this.audio.src = this.playlist[this.currentTrackIndex].url;
+	    }
+	  }, {
+	    key: 'handleTimeUpdate',
+	    value: function handleTimeUpdate() {
+	      if (!this.seekInProgress) {
+	        this.setState({
+	          displayedTime: this.audio.currentTime
+	        });
+	      }
+	    }
+	  }, {
+	    key: 'adjustDisplayedTime',
+	    value: function adjustDisplayedTime(event) {
+	      // make sure we don't select stuff in the background while seeking
+	      if (event.type === 'mousedown' || event.type === 'touchstart') {
+	        this.seekInProgress = true;
+	        document.body.classList.add('noselect');
+	      } else if (!this.seekInProgress) {
+	        return;
+	      }
+	      /* we don't want mouse handlers to receive the event
+	       * after touch handlers if we're seeking.
+	       */
+	      event.preventDefault();
+	      var boundingRect = this.audioProgressBoundingRect;
+	      var isTouch = event.type.slice(0, 5) === 'touch';
+	      var pageX = isTouch ? event.targetTouches.item(0).pageX : event.pageX;
+	      var position = pageX - boundingRect.left - document.body.scrollLeft;
+	      var containerWidth = boundingRect.width;
+	      var progressPercentage = position / containerWidth;
+	      this.setState({
+	        displayedTime: progressPercentage * this.audio.duration
+	      });
+	    }
+	  }, {
+	    key: 'seek',
+	    value: function seek(event) {
+	      /* this function is activated when the user lets
+	       * go of the mouse, so if .noselect was applied
+	       * to the document body, get rid of it.
+	       */
+	      document.body.classList.remove('noselect');
+	      if (!this.seekInProgress) {
+	        return;
+	      }
+	      /* we don't want mouse handlers to receive the event
+	       * after touch handlers if we're seeking.
+	       */
+	      event.preventDefault();
+	      this.seekInProgress = false;
+	      var displayedTime = this.state.displayedTime;
+	      if (isNaN(displayedTime)) {
+	        return;
+	      }
+	      this.audio.currentTime = displayedTime;
 	    }
 	  }, {
 	    key: 'render',
 	    value: function render() {
+	      var _this4 = this;
+
+	      var activeIndex = this.state.activeTrackIndex;
+	      var displayText = activeIndex < 0 ? null : this.playlist[activeIndex].displayText;
+
+	      var displayedTime = this.state.displayedTime;
+	      var duration = this.audio && this.audio.duration || 0;
+
+	      var elapsedTime = convertToTime(displayedTime);
+	      var fullTime = convertToTime(duration);
+	      var timeRatio = elapsedTime + ' / ' + fullTime;
+
+	      var progressBarWidth = '{ displayedTime / duration }%';
+
 	      return React.createElement(
 	        'div',
-	        { className: 'audio_player' },
-	        'Audio Player'
+	        { id: 'audio_player', className: 'audio_player', title: displayText },
+	        React.createElement(
+	          'div',
+	          { className: 'audio_controls' },
+	          React.createElement(
+	            'div',
+	            { id: 'play_pause_button',
+	              className: classNames('play_pause_button', 'audio_button', {
+	                'paused': this.state.paused
+	              }),
+	              onClick: this.togglePause.bind(this, null) },
+	            React.createElement(
+	              'div',
+	              { className: 'play_pause_inner' },
+	              React.createElement('div', { className: 'left' }),
+	              React.createElement('div', { className: 'right' }),
+	              React.createElement('div', { className: 'triangle_1' }),
+	              React.createElement('div', { className: 'triangle_2' })
+	            )
+	          ),
+	          React.createElement(
+	            'div',
+	            { id: 'skip_button',
+	              className: 'skip_button audio_button',
+	              onClick: this.skipToNextTrack.bind(this, null) },
+	            React.createElement(
+	              'div',
+	              { className: 'skip_button_inner' },
+	              React.createElement('div', { className: 'right_facing_triangle' }),
+	              React.createElement('div', { className: 'right_facing_triangle' })
+	            )
+	          )
+	        ),
+	        React.createElement(
+	          'div',
+	          { id: 'audio_progress_container',
+	            className: 'audio_progress_container',
+	            ref: function ref(_ref) {
+	              return _this4.audioProgressContainer = _ref;
+	            },
+	            onMouseDown: this.adjustDisplayedTime.bind(this),
+	            onMouseMove: this.adjustDisplayedTime.bind(this),
+	            onTouchStart: this.adjustDisplayedTime.bind(this),
+	            onTouchMove: this.adjustDisplayedTime.bind(this) },
+	          React.createElement('div', { id: 'audio_progress',
+	            className: 'audio_progress',
+	            style: { width: progressBarWidth } }),
+	          React.createElement(
+	            'div',
+	            { id: 'audio_progress_overlay', className: 'audio_progress_overlay' },
+	            React.createElement(
+	              'div',
+	              { className: 'audio_info_marquee' },
+	              React.createElement(
+	                'div',
+	                { id: 'audio_info', className: 'audio_info noselect', draggable: 'false' },
+	                displayText
+	              )
+	            ),
+	            React.createElement(
+	              'div',
+	              { id: 'audio_time_progress',
+	                className: 'audio_time_progress noselect',
+	                draggable: 'false' },
+	              timeRatio
+	            )
+	          )
+	        )
 	      );
 	    }
 	  }]);
@@ -19146,6 +19434,60 @@ return /******/ (function(modules) { // webpackBootstrap
 
 /***/ },
 /* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
+	  Copyright (c) 2016 Jed Watson.
+	  Licensed under the MIT License (MIT), see
+	  http://jedwatson.github.io/classnames
+	*/
+	/* global define */
+
+	(function () {
+		'use strict';
+
+		var hasOwn = {}.hasOwnProperty;
+
+		function classNames () {
+			var classes = [];
+
+			for (var i = 0; i < arguments.length; i++) {
+				var arg = arguments[i];
+				if (!arg) continue;
+
+				var argType = typeof arg;
+
+				if (argType === 'string' || argType === 'number') {
+					classes.push(arg);
+				} else if (Array.isArray(arg)) {
+					classes.push(classNames.apply(null, arg));
+				} else if (argType === 'object') {
+					for (var key in arg) {
+						if (hasOwn.call(arg, key) && arg[key]) {
+							classes.push(key);
+						}
+					}
+				}
+			}
+
+			return classes.join(' ');
+		}
+
+		if (typeof module !== 'undefined' && module.exports) {
+			module.exports = classNames;
+		} else if (true) {
+			// register as 'classnames', consistent with npm package name
+			!(__WEBPACK_AMD_DEFINE_ARRAY__ = [], __WEBPACK_AMD_DEFINE_RESULT__ = function () {
+				return classNames;
+			}.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+		} else {
+			window.classNames = classNames;
+		}
+	}());
+
+
+/***/ },
+/* 3 */
 /***/ function(module, exports) {
 
 	// removed by extract-text-webpack-plugin
--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,23 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const AudioPlayer = require('react-responsive-audio-player');
 
+const dir = 'audio/';
+const playlist = [
+  {
+    url: `${ dir }marty_mcpaper_theme.mp3`,
+    displayText: 'Marty McPaper: Epic Delivery Service'
+  },
+  {
+    url: `${ dir }secret_of_trash_island.mp3`,
+    displayText: 'The Secret of Trash Island'
+  },
+  {
+    url: `${ dir }in_the_hall_of_the_mountain_king.mp3`,
+    displayText: 'In the Hall of the Mountain King'
+  }
+];
+
 ReactDOM.render(
-  <AudioPlayer/>,
+  <AudioPlayer playlist={ playlist }/>,
   document.getElementById('audio_player_container')
 );
