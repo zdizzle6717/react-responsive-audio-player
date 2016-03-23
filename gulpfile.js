58fd525ebd318096ad937a2fcbc2d021ef51475e
Ben Wiley <therealbenwiley@gmail.com>
2016-03-23 05:30:48 -0400

    Build environment set up.

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var source = require('vinyl-source-stream');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('copyHTML', function () {
+  return gulp.src('./src/index.html')
+    .pipe(gulp.dest('./public/'));
+});
+ 
+gulp.task('browserify', function() {
+  return browserify('./src/js/audioplayer.js')
+    .transform('babelify', { presets: ['es2015', 'react'] })
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./src/scss/*.scss')
+    .pipe(concat('style.scss'))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({ browsers: ['> 2%'] }))
+    .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('default', ['copyHTML', 'browserify', 'sass']);
